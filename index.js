89f99801e2368d8f38d6d90d28bd1c1531f248fc
agungcaproex <agung.caproex@gmail.com>
2018-03-07 14:38:20 +0700

    snake-leader Release 1

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-function generateBoard(input){
+function generateBoard(input) {
     let result = []
     let count = input * input
 
-    for(let i=0; i<input; i++){
-        result.push([])
-        
-        if(i % 2 == 0){
-            for(let j=0; j<input; j++){
-                result[i].push(count)
-                count --
+    if (input % 2 == 0) {
+        for (let i = 0; i < input; i++) {
+            result.push([])
+
+            if (i % 2 == 0) {
+                for (let j = 0; j < input; j++) {
+                    result[i].push(count)
+                    count--
+                }
+            } else {
+                count -= (input - 1)
+                for (let k = 0; k < input; k++) {
+                    result[i].push(count)
+                    count++
+                }
+                count -= (input + 1)
             }
         }
-        else{
-            count -= (input - 1)
-            for(let k=0; k<input; k++){
-                result[i].push(count)
-                count++
+    } else {
+        for (let i = 0; i < input; i++) {
+            result.push([])
+
+            if (i % 2 != 0) {
+                for (let j = 0; j < input; j++) {
+                    result[i].push(count)
+                    count--
+                }
+            } else {
+                count -= (input - 1)
+                for (let k = 0; k < input; k++) {
+                    result[i].push(count)
+                    count++
+                }
+                count -= (input + 1)
             }
-            count -= (input + 1)
         }
     }
+
     return result
 }
 
 console.log(generateBoard(10))
+console.log(generateBoard(15))
+console.log(generateBoard(8))
