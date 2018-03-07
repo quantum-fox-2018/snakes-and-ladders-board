a342fc973eacf6c84959481aa6205342d8299613
Muhammad Ramadiansyah <muhammadramadiansyah@Muhammads-MacBook-Pro.local>
2018-03-07 15:12:43 +0700

    update tugas snake ladder board rama

--- snake-ladder-board.js
+++ snake-ladder-board.js
@@ -7,22 +7,41 @@ function generateBoard(numPapan) {
   for(let i=0; i<numPapan;i++){
 
     laddersBoard.push([]);
-    if(i % 2 == 0){
-      for(let j=0 ; j<numPapan;j++){
-        laddersBoard[i].push(counter);
-        counter--;;
+    if(i % 2 == 0  ){
+
+      if(numPapan % 2 == 0){
+        for(let j=0 ; j<numPapan;j++){
+          laddersBoard[i].push(counter);
+          counter--;;
+        }
+      }
+      else if(numPapan % 2 != 0){
+        counter = counter - (jumlahColumn-1);
+        for(let j=0 ; j<numPapan;j++){
+          laddersBoard[i].push(counter);
+          counter++;
+        }
+        counter = counter - (jumlahColumn + 1);
       }
     }
     else if(i % 2 != 0){
-      counter = counter - (jumlahColumn-1);
-      for(let j=0 ; j<numPapan;j++){
-        laddersBoard[i].push(counter);
-        counter++;
+      if(numPapan % 2 != 0){
+        for(let j=0 ; j<numPapan;j++){
+          laddersBoard[i].push(counter);
+          counter--;;
+        }
       }
-      counter = counter - (jumlahColumn + 1);
+      else if(numPapan % 2 == 0){
+        counter = counter - (jumlahColumn-1);
+        for(let j=0 ; j<numPapan;j++){
+          laddersBoard[i].push(counter);
+          counter++;
+        }
+        counter = counter - (jumlahColumn + 1);
+      }
     }
   }
   return laddersBoard;
-} 
+}
 
 console.log(generateBoard(8));
