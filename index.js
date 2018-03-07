defd5862a19deccb552f4d1b26f7f45bacf17bd2
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-07 17:11:07 +0700

    snake-play

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function generateBoard(dimensi){
+function generateBoard(dimensi, cordinat){
     let bigBoard = []
     let no = dimensi * dimensi;
     for(let r=0; r<dimensi; r++){
@@ -7,27 +7,47 @@ function generateBoard(dimensi){
             if(r % 2 != 0){
                 no -= (dimensi-1);
                 for(let c=0; c<dimensi; c++){
-                    rowBoard.push(no);
-                    no++;
+                    if(no == cordinat){
+                        rowBoard.push('(X)');
+                        no++;
+                    } else {
+                        rowBoard.push(no);
+                        no++;
+                    }
                 }
                 no -= (dimensi+1);
             } else {
                 for(let c=0; c<dimensi; c++){
-                    rowBoard.push(no);
-                    no--;
+                    if(no == cordinat){
+                        rowBoard.push('(X)');
+                        no--;
+                    } else {
+                        rowBoard.push(no);
+                        no--;
+                    }
                 }
             }
         } else {
             if(r % 2 != 0){
                 for(let c=0; c<dimensi; c++){
-                    rowBoard.push(no);
-                    no--;
+                    if(no == cordinat){
+                        rowBoard.push('(X)');
+                        no--;
+                    } else {
+                        rowBoard.push(no);
+                        no--;
+                    }
                 }
             } else {
                 no -= (dimensi-1);
                 for(let c=0; c<dimensi; c++){
-                    rowBoard.push(no);
-                    no++;
+                    if(no == cordinat){
+                        rowBoard.push('(X)');
+                        no++;
+                    } else {
+                        rowBoard.push(no);
+                        no++;
+                    }
                 }
                 no -= (dimensi+1);
             }
@@ -38,4 +58,28 @@ function generateBoard(dimensi){
     return bigBoard;
 }
 
-console.log(generateBoard(8))
+function sleep(milliseconds){
+    var start = new Date().getTime();
+    for(var i=0; i<1e7; i++){
+        if((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
+
+let dimensionBoard = 10
+
+
+let countStep = 0;
+let longBoard = dimensionBoard * dimensionBoard;
+for(let i=0; i<longBoard; i++){
+    if(countStep >= longBoard){
+        console.log(generateBoard(dimensionBoard, longBoard))
+        break;
+    }
+    console.log(generateBoard(dimensionBoard, countStep))
+    sleep(2000)
+    countStep += Math.floor(Math.random()*6 + 1);
+    console.log('\n')
+}
+
