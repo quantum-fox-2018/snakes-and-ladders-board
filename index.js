158b7fa6d23a6855bd38864f10365cf81c6a3251
Zena <zena.vilenia@gmail.com>
2018-03-07 15:03:57 +0700

    ganti ga pake unshift

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 function generateBoard(num) {
   let arrBoard = boardTemplate(num);
   let angka = 1;
-
+  let arr = buatArray(num);
   for(let i = arrBoard.length-1; i >= 0; i--) {
+    arr = [];
     for(let j = arrBoard.length-1; j >= 0; j--) {
       if(i%2 === 0) {
-        arrBoard[i].unshift(angka);
+        arr[j] = angka;
       } else {
         arrBoard[i].push(angka);
       }
       angka++;
     }
+    if(i%2 === 0) {
+      arrBoard[i] =  arr;
+    }
   }
   return arrBoard;
+}
+
+function buatArray(num) {
+  let arr = [];
+  for(let i = 0; i < num; i++) {
+    arr.push([]);
+  }
+
+  return arr;
 }
 
 function boardTemplate(num) {
@@ -25,4 +38,4 @@ function boardTemplate(num) {
   return arrHasil;
 }
 
-console.log(generateBoard(10));
+console.log(generateBoard(8));
