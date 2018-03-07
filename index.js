d1c200404db6e7f7baa4f38da6f6b3cbf7162bf2
richoandy <richoandy@gmail.com>
2018-03-07 03:34:54 -0500

    update code push method only

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 function generateBoard(size){
-  var boardCount = size*size;
+
   var board = [];
-  var flag = "plus";
-  for (var i = 0; i < size; i++) {
-    var tempArr = [];
-      for (var j = 0; j < size; j++) {
-          tempArr.push(boardCount);
-          boardCount--;
+
+  for (let i = 0; i < size; i++) {
+      board.push([])
+  }
+
+
+  var boardCount = 1;
+  var flag = 0;
+  for (var j = board.length-1; j >= 0; j--) {
+    if (flag%2 === 1) {
+      var temp = [];
+      for (var k = 0; k < size; k++) {
+        temp.push(boardCount)
+        boardCount++
       }
-      board.push(tempArr)
-  }
-  var counter = 1;
-  for (var k = board.length; k >= 0; k--) {
-    if (counter%2 === 0) {
-      var temp = board[k]
-      temp = temp.reverse();
-      board.splice(k, 1, temp)
+      temp = reverseArray(temp)
+      for (var m = 0; m < temp.length; m++) {
+        board[j].push(temp[m])
+      }
     }
-    counter++;
+    else{
+      for (var k = 0; k < size; k++) {
+        board[j].push(boardCount)
+        boardCount++
+      }
+    }
+    flag++
   }
+
+  function reverseArray(arr){
+    var resultArr = [];
+    for (var l = arr.length-1; l >= 0; l--) {
+      resultArr.push(arr[l])
+    }
+    return resultArr
+  }
+
+  // var counter = 1;
+  // for (var k = board.length; k >= 0; k--) {
+  //   if (counter%2 === 0) {
+  //     var temp = board[k]
+  //     temp = temp.reverse();
+  //     board.splice(k, 1, temp)
+  //   }
+  //   counter++;
+  // }
   return board;
 }
 
-console.log(generateBoard(12))
+console.log(generateBoard(10))
